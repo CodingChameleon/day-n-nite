adc42bef6dea5c84ae4537281f423b11d2be1e51
Naia McNatt <nmcnatt@wlapcs.org>
2017-03-07 20:09:30 +0000

    oohhh

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ document.getElementById("orb").addEventListener("click",function(){
     changeToNight()
 });
 function changeToNight(){
-    document.getElementById("sky").className = "night";
-    document.getElementById("orb").className = "moon";
+    if(document.getElementById("orb").className == "sun"){
+        document.getElementById("sky").className = "night";
+        document.getElementById("orb").className = "moon";
+        document.getElementById("moonspot1").className = "dark";
+        document.getElementById("moonspot2").className = "dark";
+        document.getElementById("moonspot3").className = "dark";
+}
+else if(document.getElementById("orb").className == "moon"){
+    document.getElementById("sky").className = "day";
+    document.getElementById("orb").className = "sun";
+    document.getElementById("moonspot1").className = "light";
+        document.getElementById("moonspot2").className = "light";
+        document.getElementById("moonspot3").className = "light";
+    }
 }
