3e4907e3b393962629e7ada154063a4eedea0803
Naia McNatt <nmcnatt@wlapcs.org>
2017-03-07 19:39:18 +0000

    NOOOOO!

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("orb").addEventListener("click",function(){
+    changeToNight()
+});
+function changeToNight(){
+    document.getElementById("sky").className = "night";
+    document.getElementById("orb").className = "moon";
+}
